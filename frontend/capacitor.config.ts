be95a7593e2062885dd748f8aa894c95fcad6367
Aaditya kumar sah <shahsudha259@gmail.com>
2025-10-14 14:18:46 +0545

    readme updated

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mycompany.botanicdefenders',
+  appName: 'Botanic Defenders',
+  webDir: 'public'
+};
+
+export default config;
